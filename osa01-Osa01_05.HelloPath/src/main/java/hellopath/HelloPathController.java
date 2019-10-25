d9022abfe5f9fe43f3eeac4fc5f1033ffa7589d3
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-10-25 16:38:51 +0300

    05.HelloPath

--- osa01-Osa01_05.HelloPath/src/main/java/hellopath/HelloPathController.java
+++ osa01-Osa01_05.HelloPath/src/main/java/hellopath/HelloPathController.java
@@ -6,6 +6,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloPathController {
-
-
+/*
+    @GetMapping("*")
+    @ResponseBody
+    public String home() {
+        return "Hello World!";
+    }
+*/
+    @GetMapping("/path")
+    @ResponseBody
+    public String path() {
+        return "Oikein!";
+    }
 }
