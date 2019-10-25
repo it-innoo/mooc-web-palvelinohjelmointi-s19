39fb9a561ce4bfbcf8e792756587878b1ef03583
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-10-25 16:03:29 +0300

    03.HelloRedirectLoop

--- osa01-Osa01_03.HelloRedirectLoop/src/main/java/HelloRedirectLoop.java
+++ osa01-Osa01_03.HelloRedirectLoop/src/main/java/HelloRedirectLoop.java
@@ -9,5 +9,35 @@ public class HelloRedirectLoop {
     public static void main(String[] args) throws Exception {
         ServerSocket server = new ServerSocket(8080);
 
+        int laskuri = 1;
+
+        while (true) {
+            Socket asiakas = server.accept();
+            System.out.println("Pyyntö: " + laskuri);
+            laskuri++;
+
+            Scanner s = new Scanner(asiakas.getInputStream());
+
+            String p = s.nextLine();
+
+            if (p.contains("quit")) {
+                break;
+            }
+
+ 
+
+            PrintWriter kirjoittaja = new PrintWriter(asiakas.getOutputStream());
+
+            kirjoittaja.write("HTTP/1.1 302 Found\r\n");
+            kirjoittaja.write("Location: http://localhost:8080\r\n");
+            kirjoittaja.flush();
+
+ 
+
+            kirjoittaja.close();
+            asiakas.close();
+            s.close();
+        }
+        server.close();
     }
 }
