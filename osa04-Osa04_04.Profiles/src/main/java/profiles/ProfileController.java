0dce51a1196181410dc53fa32be425cfba3f9eb5
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-11-18 17:32:52 +0200

    Osa04_04.Profiles

--- osa04-Osa04_04.Profiles/src/main/java/profiles/ProfileController.java
+++ osa04-Osa04_04.Profiles/src/main/java/profiles/ProfileController.java
@@ -2,20 +2,26 @@ package profiles;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ProfileController {
 
+    @Value("${spring.profiles.active:test}")
+    private String activeProfiles;
 
     @ResponseBody
     @GetMapping("/profile")
     public String getProfile() {
-        return "Hello world!";
+        return activeProfiles;
     }
 
-    public String home() {
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("profiles", activeProfiles);
+        
         return "index";
     }
 
